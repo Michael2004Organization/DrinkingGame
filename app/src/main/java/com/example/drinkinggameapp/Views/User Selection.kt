bc9a146d42bdcd54c892c09e3598e0d9fd1d09a6
Michael Zimmermann <zimmermann@ake-technologies.de>
2025-02-17 15:56:53 +0100

    Add View 'User Selection'

---
+++ app/src/main/java/com/example/drinkinggameapp/Views/User Selection.kt
@@ -0,0 +1,136 @@
+package com.example.drinkinggameapp.Views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.drinkinggameapp.R
+
+@Preview
+@Composable
+fun UserSelection() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.darkBlue)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                //.clip(shape = RoundedCornerShape(20.dp))
+                .padding(start = 20.dp, end = 20.dp)
+                .background(Color.White, shape = RoundedCornerShape(15.dp))
+                .height(200.dp),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        Color.Black, shape = RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp)
+                    )
+                    .height(30.dp), verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = Modifier.padding(start = 5.dp),
+                    text = "Spieler hinzufügen: ",
+                    color = Color.White,
+                )
+            }
+
+            addPlayer()
+        }
+    }
+}
+
+@Composable
+fun addPlayer() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(0.2f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                Icons.Rounded.Person,
+                contentDescription = "",
+                modifier = Modifier.padding(start = 5.dp)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Absolute.Right
+        ) {
+            Icon(
+                Icons.Rounded.Add,
+                contentDescription = "",
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(50.dp)
+                    .padding(end = 10.dp),
+            )
+        }
+    }
+}
+
+@Composable
+fun addedPlayer() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(0.2f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                Icons.Rounded.Person,
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(start = 5.dp)
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Spieler 1",
+            )
+        }
+    }
+}
